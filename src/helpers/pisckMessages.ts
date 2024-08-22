ed335ff96ba5a579edabd7717194d23d1225764f
Ivan-rom <rom_1van@icloud.com>
2024-08-22 20:38:04 +0600

    refactor: remove unused helper

--- src/helpers/pisckMessages.ts
+++
@@ -1,7 +0,0 @@
-export function pickMessages<T extends object, K extends keyof T>(
-  base: T,
-  ...keys: K[]
-): Pick<T, K> {
-  const entries = keys.map((key) => [key, base[key]]);
-  return Object.fromEntries(entries);
-}
