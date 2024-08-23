3ea8c37206d8a1f839abc390d78ddb1de3ba6d52
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-08-23 20:42:26 +0500

    feat: added welcome page template and component for it.

---
+++ src/app/[lang]/welcome/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import styles from './page.module.css';
+import { useTranslations } from 'next-intl';
+import WelcomeScreen from '@/components/WelcomeScreen/WelcomeScreen.tsx';
+import { Routes } from '@/helpers/enums.ts';
+
+export default function Welcome() {
+  const t = useTranslations();
+  const isAuthenticated = false;
+  const userName = 'User Userovich';
+
+  return (
+    <section className={styles.welcome}>
+      <WelcomeScreen
+        title={
+          isAuthenticated
+            ? t('HomePage.welcomeBack').concat(userName)
+            : t('HomePage.title')
+        }
+        description={
+          isAuthenticated
+            ? t('HomePage.description')
+            : t('HomePage.unauthorized')
+        }
+        isAuthenticated={isAuthenticated}
+        userName={isAuthenticated ? userName : null}
+        links={
+          isAuthenticated
+            ? []
+            : [
+                { href: Routes.signIn, title: t('SignIn.title') },
+                { href: Routes.signUp, title: t('SignUp.title') },
+              ]
+        }
+      />
+    </section>
+  );
+}
