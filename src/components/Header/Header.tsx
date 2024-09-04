be1e47e2d1ce164fdd2b9a29ae2c9b78025084cb
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-09-04 12:42:28 +0500

    feat: added fixed animated header and corrected several other styles and Header component's setting

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,35 @@
+"use client";
+
 import styles from './Header.module.css';
 import HeaderButtons from '@/components/HeaderButtons/HeaderButtons';
-import { Link } from '@/helpers/navigation.ts';
+import { Link } from '@/helpers/navigation';
 import LanguageToggler from '@/components/LanguageToggler/LanguageToggler';
-import { Routes } from '@/helpers/enums.ts';
+import { Routes } from '@/helpers/enums';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isFixed, setFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setFixed(true);
+      } else {
+        setFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`
+      ${styles.header} ${isFixed ? styles.fixed : ''}
+    `}>
       <Link href={Routes.home} className={styles.logo}>
         Unit
       </Link>
