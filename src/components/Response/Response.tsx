6b2069c452b91f32d66cfdf5c03631069458fbc1
Ivan-rom <rom_1van@icloud.com>
2024-09-06 19:37:31 +0600

    refactor: remove commented code

--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -1,17 +1,12 @@
 'use client';
 
-// import { useParams, useSearchParams } from 'next/navigation';
-// import { useEffect, useMemo, useState } from 'react';
 import styles from './response.module.css';
 import CodeEditor from '../CodeEditor/CodeEditor';
 import classNames from 'classnames';
 import { useTranslations } from 'next-intl';
 import { useStatusCodeClassName } from '@/hooks/useStatusCodeClassName';
 import { extensions } from './editorExtensions';
-// import { makeRequest } from '@/services/request';
 import Loader from '../Loader/Loader';
-
-// const INITIAL_RESPONSE_VALUE = { status: 0, body: '' };
 
 type Props = {
   value: { status: number; body: string };
@@ -20,33 +15,8 @@ type Props = {
 
 function Response({ value, isLoading }: Props) {
   const t = useTranslations('Client');
-  // const params = useParams();
-  // const request = params.request || [];
-  // const [method, urlBase64, bodyBase64] = request;
 
-  // const searchParams = useSearchParams();
-  // const headers = useMemo(() => {
-  //   const headersObject: { [key: string]: string } = {};
-  //   searchParams.forEach((value, key) => {
-  //     headersObject[key] = value;
-  //   });
-  //   return headersObject;
-  // }, [searchParams]);
-
-  // const [responseObject, setResponseObject] = useState(INITIAL_RESPONSE_VALUE);
-  // const [isLoading, setIsLoading] = useState(true);
   const codeClassName = useStatusCodeClassName(value.status);
-
-  // useEffect(() => {
-  //   makeRequest(urlBase64, bodyBase64, method, headers)
-  //     .then((res) => setResponseObject(res))
-
-  //     //TODO: show error result to user
-  //     // use toastify for example
-  //     .catch((error) => setResponseObject(error))
-
-  //     .finally(() => setIsLoading(false));
-  // }, [bodyBase64, method, urlBase64, headers]);
 
   return (
     <div className={styles.response}>
