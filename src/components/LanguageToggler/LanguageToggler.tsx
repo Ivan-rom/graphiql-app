4c6792e81f3743170668176e4eeb71664b1c8f49
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-09-13 23:30:50 +0500

    fix: fixed lang toggle

--- src/components/LanguageToggler/LanguageToggler.tsx
+++ src/components/LanguageToggler/LanguageToggler.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import styles from './LanguageToggler.module.css';
-import { useRouter, usePathname } from '@/helpers/navigation';
+import { usePathname } from '@/helpers/navigation';
 import { locales } from '@/helpers/constants';
-import { useParams } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 
 function LanguageToggler() {
   const router = useRouter();
@@ -13,7 +13,9 @@ function LanguageToggler() {
 
   const toggleLanguage = () => {
     const newLocale = locale === locales[0] ? locales[1] : locales[0];
-    router.push(newLocale + pathname);
+    const newPathname = pathname.replace(`/${locale}`, '');
+
+    router.push(`/${newLocale}${newPathname}`);
   };
 
   return (
