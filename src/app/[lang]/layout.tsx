5f4e09a61e832ad8401b5bf4a3e136ea3a983988
Ivan-rom <rom_1van@icloud.com>
2024-09-14 12:06:12 +0600

    feat: implement user redirect

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,6 +5,7 @@ import StoreProvider from '../StoreProvider/StoreProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import { ReactNode } from 'react';
+import AuthWrapper from '@/components/AuthWrapper/AuthWrapper';
 
 type Props = {
   params: {
@@ -22,7 +23,9 @@ export default async function Layout({ children, params }: Props) {
         <StoreProvider>
           <NextIntlClientProvider messages={messages}>
             <Header />
-            <main className={styles.main}>{children}</main>
+            <main className={styles.main}>
+              <AuthWrapper>{children}</AuthWrapper>
+            </main>
             <Footer />
           </NextIntlClientProvider>
         </StoreProvider>
